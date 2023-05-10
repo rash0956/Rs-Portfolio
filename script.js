5daa527a84934766b99f3301ad30e345af2aa92e
Rashmi Sandaruwini <118651162+rash0956@users.noreply.github.com>
2023-05-11 00:07:40 +0530

    change portfolio and add my work and contact section fix. add new things.

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+/*=============== toggle icon nvbar ===============*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/*=============== scroll section ===============*/
+
+let section = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop = 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+           
+            //active navbar links
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+
+
+
+    
+    // sticky header
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and number when click navbar links 
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*=============== About ===============*/
 
 var tablinks = document.getElementsByClassName("tab-links");
@@ -13,4 +75,23 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+/*=============== contact ===============*/
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyhPESKrDnTM8M9HnDazcpwSLXAPXHoIcT98eVt0-JzZhKqxAmMBZeNeQx0Qn6AlAzEWw/exec'
+  const form = document.forms['contact']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
 
